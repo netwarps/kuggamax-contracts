82f91ef4d2d35556be2d616ec381cf05db5ff920
wang <231629659@qq.com>
2022-12-14 11:29:47 +0800

    Adjusted the permit func params.

--- test/token.js
+++ test/token.js
@@ -1,19 +1,26 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
-describe("Token contract", function() {
-  it("Deployment should assign the total supply of tokens to the owner", async function() {
-    const [owner] = await ethers.getSigners();
+function tokenContract() {
+  describe("Token20 contract", function () {
+    it("Deployment should assign the total supply of tokens to the owner", async function () {
+      const [owner] = await ethers.getSigners();
 
-    const Token = await ethers.getContractFactory("Token");
+      const Token = await ethers.getContractFactory("Token20");
 
-    const hardhatToken = await Token.deploy(1000000);
+      const hardhatToken = await Token.deploy(1000000);
 
-	console.log("Token address:", hardhatToken.address);
+      console.log("Token address:", hardhatToken.address);
 
-    const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+      const ownerBalance = await hardhatToken.balanceOf(owner.address);
+      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
 
-	console.log('tested')
+      console.log('tested')
+    });
   });
-});
+}
+
+
+module.exports = {
+  tokenContract,
+}
